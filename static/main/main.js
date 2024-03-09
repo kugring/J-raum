2865a9bef76d09b6f4ec058dd5a68ba7212a2c6f
kugring <wogur8243@gmail.com>
2024-03-09 17:45:49 +0900

    확대 축소 방지 코드 추가4

--- static/main/main.js
+++ static/main/main.js
@@ -1681,3 +1681,18 @@ function orderData(order_person, phoneNumber) {
 	}, 500)
 
 }
+
+document.addEventListener('touchmove', function (event) {
+	if (event.scale !== 1) {
+		event.preventDefault();
+	}
+}, false);
+
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+	var now = (new Date()).getTime();
+	if (now - lastTouchEnd <= 300) {
+		event.preventDefault();
+	}
+	lastTouchEnd = now;
+}, false);
